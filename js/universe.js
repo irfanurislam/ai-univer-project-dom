03ed7005b37e47aa270a411de2da20a4cb1b06a7
irfanurislam <mdirfanurislam@gmail.com>
2023-03-04 20:05:29 +0600

    call fetch

--- js/universe.js
+++ js/universe.js
@@ -1,1 +1,13 @@
-console.log('jlw');
+const loaduser = async () => {
+    try {
+      const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`)
+      const data = await res.json();
+      console.log(data.data.tools);
+      displayai(data.data.tools.slice(0, 6));
+    }
+    catch (error) {
+      console.log(error);
+    }
+  
+  }
+  loaduser();
