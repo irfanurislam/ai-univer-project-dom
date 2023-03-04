3a74c27087489f61de8ae584b58b7ed7db799cb4
irfanurislam <mdirfanurislam@gmail.com>
2023-03-04 20:16:22 +0600

    card load

--- js/universe.js
+++ js/universe.js
@@ -11,3 +11,52 @@ const loaduser = async () => {
   
   }
   loaduser();
+
+  const displayai = (data) => {
+    console.log(data);
+    const aicontainer = document.getElementById('ai_contain');
+    aicontainer.innerHTML = "";
+  
+    
+    //array loop single card value get
+    data.forEach(card => {
+      console.log(card);
+    
+      const { id, image, name, features, published_in } = card;
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `
+  
+    <div class="card h-100 p-3">
+                    <img src="${image}" class="card-img-top rounded-2" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title fw-bold p-0">Feature</h5>
+                      <ol type="1" class="card-text p-2" id="featuresItem">
+                      <li class="text-secondary"></li>
+                      <li class="text-secondary"></li>
+                  <li class="text-secondary"></li>
+                        </ol>
+                      </div>
+                      <hr class="px-2">
+                      <div class="card-footer border-0 bg-body">
+                       <div class="d-flex justify-content-between align-items-center">
+                          <div>
+                              <h4></h4>
+                             <div class = "d-flex gap-3 align-items-center mt-2">
+                             
+                              <p></p>
+                             </div>
+                          </div>
+                          
+                       </div>
+                      </div>
+                    </div>
+  
+  
+  
+      `;
+      aicontainer.appendChild(div);
+    });
+  
+  
+  }
